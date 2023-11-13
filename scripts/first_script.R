5ba3a2e7b5a081c2966145a1c582ff46eccf69c9
Rosalie Turkstra <r.turkstra@student.rug.nl>
2023-11-13 12:05:37 +0100

    Project initialized with renv

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,1 +1,11 @@
 # my first github synced script
+library(renv)
+# restore the versions of the packages used to develop this project
+renv::restore() # only INSIDE this project, the right version of each ...
+# project will be installed, so that it is reproducible
+
+x <- c(1,2,3,4,5)
+y <- c(1,4,3,6,9)
+data <- data.frame(x,y)
+data
+
